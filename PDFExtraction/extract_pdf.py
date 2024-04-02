0da9328bc5b5440a80fedc56890784e2991b0085
Aaditya18SP <aaditya.sp@somaiya.edu>
2024-04-02 23:09:22 +0530

    getting main topics from pdfs.Need to get sub-topics and its text

--- PDFExtraction/extract_pdf.py
+++ PDFExtraction/extract_pdf.py
@@ -5,8 +5,10 @@ doc= fitz.open("../PDFExtraction/pdfs/AnnualReport1.pdf")
 
 print(f"total pages :{doc.page_count}")
 
-for i in range(0,doc.page_count): 
+for i in range(5,10): 
     current_page= doc.load_page(i)
     page_on_text=current_page.get_text()
     print(f"text on page {i}")
-    print(page_on_text) 
+    print(page_on_text)
+
+

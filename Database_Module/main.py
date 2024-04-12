fa261b8995d6eb5b95563c4800e9547336d0877d
HRUSHIKESH SHINDE <hrushiop@gmail.com>
2024-04-12 08:30:03 +0000

    reupload

--- Database_Module/main.py
+++ Database_Module/main.py
@@ -1,1 +1,9 @@
-import mongo from pymongo
+import pymongo
+
+myclient = pymongo.MongoClient('mongodb+srv://hrushiop:oq16yL7AoXMn1c3x@cluster0.h4boskd.mongodb.net/')
+mydb = myclient['test']
+mycol = mydb["extracted_data"]
+mydict = { "name": "John", "address": "Highway 37" }
+x = mycol.insert_one(mydict)
+
+print(x)
